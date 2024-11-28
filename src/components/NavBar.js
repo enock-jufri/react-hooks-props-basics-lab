2daf3a786840f7308de697c66697a457276ff851
enock-jufri <enockjufri@gmail.com>
2024-11-28 12:21:45 +0300

    props

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,8 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
+      <a href="home">Home</a>
+      <a href="about">About</a>
     </nav>
   );
 }
